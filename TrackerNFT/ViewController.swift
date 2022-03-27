c94d4bcf4c5916a9c79cd4e3a3bd3f71bf08f406
Oleg Kasarin <oleg.kasarin@oxagile.com>
2022-03-27 13:44:09 +0300

    Initial Commit

---
+++ TrackerNFT/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrackerNFT
+//
+//  Created by Oleg Kasarin on 27.03.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
